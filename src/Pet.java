7e2bb50220aff868cfbe0d8986471e9c52469a4f
Baigopa <yogiadityanarotama@gmail.com>
2024-03-04 14:28:56 +0700

    first commit

---
+++ src/Pet.java
@@ -0,0 +1,15 @@
+public class Pet {
+    String nama = "Tokio";
+    int umur = 1;
+    String ras = "Cupang";
+    String makanan = "Cacing";
+    String warna = "Hitam";
+
+    public void displayPet() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Ras: " + ras);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Warna: " + warna);
+    }
+}
